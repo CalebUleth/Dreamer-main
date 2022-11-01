7a2720951b99feb67c6fae56bbb32b636fd17c6e
CalebUleth <caleb.woelcke@uleth.ca>
2022-11-01 13:40:18 -0600

    Fixes some bugs

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -18,14 +18,20 @@ var app = new Framework7({
   ],
 
 });
+//config
 var $$ = Dom7;
 var mainView = app.views.create('.view-main')
 
+var textEditor = app.textEditor.create({el: '#text'});
+
+//global vars
 const entries = [];
 var index = 0;
 
 const date = new Date();
 
+
+//functions
 function setIndex(aInt){
   console.log(aInt);
   index = aInt;
@@ -33,24 +39,26 @@ function setIndex(aInt){
 function loadSave(){
   var temp = "";
   temp = localStorage.getItem('localSave');
-  if(temp == null){
-
+  
+  if(temp != null){
+    console.log("Save Contents: " +temp);
+    entries = temp.split(';');
   }
   else{
-    entries = temp.split(';');
+    console.log("Save Not Found" + temp);
   }
   
 }
 function save(){
-    //addEntry(getCurrentDate+":"+.getValue());
+    addEntry(textEditor.getValue());
     console.log("saved");
     saveAll();
 }
 function saveAll(){
-  var length = entries.size;
+  var length = entries.length;
   var i = 0;
   var temp = "";
- 
+ console.log(length);
   while (i< length){
     temp+=entries[i]+";";
     i++;
@@ -61,12 +69,12 @@ function addEntry(aString){
   var temp = getCurrentDate+":"+aString;
   entries.unshift(temp);
 }
-function extractDate(aString, aIndex){
-  const splitResult = entries[aIndex].split(':');
+function extractDate(aString){
+  const splitResult = aString.split(':');
   return splitResult[0];
 }
-function extractContents(aString, aIndex){
-  const splitResult = entries[aIndex].split(':');
+function extractContents(aString){
+  const splitResult = aString.split(':');
   return splitResult[1];
 }
 
@@ -81,34 +89,35 @@ function generateCards(){
   var temp2;
   if(length == 0){
     console.log("No save found");
-      return "<div class=\"card\"><div class=\"card-header date\"><div id=\""+getCurrentDate()+"\">Date</div></div><div class=\"card-content card-content-padding\">You have not made any entries yet. Press the New button to get started.</div></div>";
+      return "<div class=\"card\"><div class=\"card-header date\"><div>"+getCurrentDate()+"</div></div><div class=\"card-content card-content-padding\">You have not made any entries yet. Press the New button to get started.</div></div>";
   }
   else{
     console.log("Save found");
+    
     while (i< length){
       index = i;
-      temp2 = extractContents();
+      temp2 = extractContents(entries[i]);
       if(temp2.length > 200){
         temp2 = temp2.substring(0, 200)+"...";
       }
-      temp+="<a href=\"/viewentry/\" onclick=\"setIndex("+i+")\"><div class=\"card\"><div class=\"card-header date\"><div id=\""+extractDate()+"\">Date</div></div><div class=\"card-content card-content-padding\">"+temp2+"</div></div></a>";
+      console.log("Entry: "+entries[i]);
+      temp+="<a href=\"/viewentry/\" onclick=\"setIndex("+i+")\"><div class=\"card\"><div class=\"card-header date\"><div>"+extractDate(entries[i])+"</div></div><div class=\"card-content card-content-padding\">"+temp2+"</div></div></a>";
       i++;
     }
+    console.log("temp: " +temp);
     return temp;
   }
 }
-//Handle Events
-$("#save").on("click", function () {
-  //addEntry(getCurrentDate+":"+.getValue());
-  console.log("saved");
-  saveAll();
-})
-
-//pageloaders
-$$(document).on('page:init','.page[data-name="home"]', function(){
+function loadMainPage(){
   console.log("reading save")
   loadSave();
   document.getElementById("cards").innerHTML = generateCards();
+}
+//Handle Events
+
+//pageloaders
+$$(document).on('page:init','.page[data-name="home"]', function(){
+  loadMainPage();
 })
 
 $$(document).on('page:init','.page[data-name="newentry"]', function(){
@@ -118,5 +127,8 @@ $$(document).on('page:init','.page[data-name="newentry"]', function(){
 
 $$(document).on('page:init','.page[data-name="viewentry"]', function(){
   console.log("view entry");
-  document.getElementById("cards").innerHTML = "<div class=\"card\"><div class=\"card-header date\"><div id=\""+extractDate()+"\">Date</div></div><div class=\"card-content card-content-padding\">"+extractContents()+"</div></div>";
+  if(entries.length < 0){
+    document.getElementById("entry").innerHTML = "<div class=\"card\"><div class=\"card-header date\"><div id=\""+extractDate()+"\">Date</div></div><div class=\"card-content card-content-padding\">"+extractContents()+"</div></div>";
+  }
 })
+loadMainPage();
